dee1cd57968397c8c0720accfa8477ee8f576048
Nikita Sufyanov <niksuf1@yandex.ru>
2022-07-01 03:42:49 +0300

    Code format fix

--- MergeSort.py
+++ MergeSort.py
@@ -1,13 +1,15 @@
 from random import random, seed, randint
 from time import time
 
-def isSorted(array, size):                                                              # Проверка на упорядоченность
+
+def isSorted(array, size):                                                                  # Проверка на упорядоченность
     for i in range (0, size - 1):
         if array[i] > array[i + 1]:
             return False
     return True
 
-def printArray(array, size):                                                            # Вывод массива
+
+def printArray(array, size):                                                                # Вывод массива
     if isSorted(array, size):
         print('Sorted!')
     else:
@@ -17,13 +19,15 @@ def printArray(array, size):                                                    
         else: break
     print('\n')
 
-def mergeSort(array, left, right):                                                      # Сортировка массива
+
+def mergeSort(array, left, right):                                                          # Сортировка массива
     if left >= right:
         return
     mid = (left + right) // 2
     mergeSort(array, left, mid)
     mergeSort(array, mid + 1, right)
     merge(array, left, right, mid)
+
 
 def merge(array, left, right, mid):
     left_copy = array[left:mid + 1]
@@ -48,21 +52,24 @@ def merge(array, left, right, mid):
         right_copy_index += 1
         sorted_index += 1
 
-seed(time())                                                                            # Инициализация таймера
-print('Input array size: ')
-size = int(input())                                                                     # Размер массива
-random_array = []
-for i in range (0, size):                                                               # Заполнение массива случайными числами
-    random_array.append(random() * size)
-printArray(random_array, size)
 
-l = 0
-r = size - 1
+def main():
+    seed(time())                                                                            # Инициализация таймера
+    print('Input array size: ')
+    size = int(input())                                                                     # Размер массива
+    random_array = []
+    for i in range (0, size):                                                               # Заполнение массива случайными числами
+        random_array.append(random() * size)
+    printArray(random_array, size)
+    l = 0
+    r = size - 1
+    t1 = time()
+    mergeSort(random_array, l, r)                                                           # Сортировка
+    t2 = time()
+    elapsed = 1000 * (t2-t1)                                                                # Время, затраченное на операцию, мс
+    printArray(random_array, size)
+    print('Time: ', elapsed, 'ms')
 
-t1 = time()
-mergeSort(random_array, l, r)                                                           # Сортировка
-t2 = time()
-elapsed = 1000 * (t2-t1)                                                                # Время, затраченное на операцию, мс
 
-printArray(random_array, size)
-print('Time: ', elapsed, 'ms')
+if __name__ == '__main__':
+    main()
